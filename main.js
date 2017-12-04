cc96ae8d052377bc02f0a1e8de34b1a28f3ab487
salvationz <kkl@one-asset.com>
2017-12-04 10:21:10 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+var app = express();
+var port = (process.env.PORT || 4000);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+    res.send('Hello');
+});
+
+app.listen(port, function(){
+    console.log('run app at port : ', port);
+});
