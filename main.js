761c9dddb61d9e9a6ba1aa4758eb866caf4074b2
salvationz <kkl@one-asset.com>
2017-12-04 15:11:13 +0700

    refractor

--- main.js
+++ main.js
@@ -7,33 +7,38 @@ var port = (process.env.PORT || 4000);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('V1.0.2' + process.env.version);
 });
 
 app.post('/webhook', function (req, res) {
-    var text = req.body.events[0].message.text
-    var sender = req.body.events[0].source.userId
-    var replyToken = req.body.events[0].replyToken
+    var text = req.body.events[0].message.text;
+    var type = req.body.events[0].message.type;
+    var sender = req.body.events[0].source.userId;
+    var replyToken = req.body.events[0].replyToken;
     console.log(text, sender, replyToken)
     console.log(typeof sender, typeof text)
     // console.log(req.body.events[0])
     if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-        sendText(sender, text)
+        sendText(sender, text, type)
     }
     res.sendStatus(200)
 });
 
-function sendText(sender, text) {
+function sendText(sender, text, type) {
     let data = {
         to: sender,
         messages: [
             {
                 type: 'text',
-                text: 'test'
+                text: 'test ' + type
             }
         ]
     }
+    sendRequest(data);
+}
+
+function sendRequest(data) {
     request({
         headers: {
             'Content-Type': 'application/json',
